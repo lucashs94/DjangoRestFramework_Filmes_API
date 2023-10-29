dceddec36f639754f9d6bf0b364c39d0f6dbc6b1
lucashs94 <h7.lucas@gmail.com>
2023-10-29 19:59:35 -0300

    creating URL pattern for api main file and for each app

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
     
 ]
